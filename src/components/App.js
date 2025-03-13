c3bd7829bf9a08d76578dffca8c6ade3652852b0
barendan <barendan@gmail.com>
2025-03-13 11:15:15 +0000

    Minor tweaks

--- src/components/App.js
+++ src/components/App.js
@@ -33,3 +33,4 @@ export default App;// TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
+// TODO: Improve component performance
