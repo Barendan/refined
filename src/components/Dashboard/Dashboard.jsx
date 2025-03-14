9975119d5723f855bafabee914c0ce34d6bcde17
barendan <barendan@gmail.com>
2025-03-14 23:43:03 +0000

    Improving performance

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -49,3 +49,4 @@ export default Dashboard;// TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
+// TODO: Improve component performance
