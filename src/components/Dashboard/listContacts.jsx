e316b19861531bfb8f3159b93527c812106ca2a1
barendan <barendan@gmail.com>
2025-03-14 11:15:49 +0000

    Enhancing readability

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -165,3 +165,4 @@ export default ContactList// TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
+// TODO: Improve component performance
