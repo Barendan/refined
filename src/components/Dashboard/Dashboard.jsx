b25eec5834a5d1d5f4225a6f9a9af6c7e4fbed0b
barendan <barendan@gmail.com>
2025-03-14 20:30:33 +0000

    Minor tweaks

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -46,3 +46,4 @@ export default Dashboard;// TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
+// TODO: Improve component performance
