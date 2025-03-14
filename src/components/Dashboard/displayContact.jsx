2f7e1016e3ee65995d47ff92c777ccc6b23a7b09
barendan <barendan@gmail.com>
2025-03-14 16:46:20 +0000

    Minor tweaks

--- src/components/Dashboard/displayContact.jsx
+++ src/components/Dashboard/displayContact.jsx
@@ -151,3 +151,4 @@ export default DisplayContact;// TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
+// TODO: Improve component performance
