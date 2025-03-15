c4c709c0974fb0d8d750f6b26f462661312def87
barendan <barendan@gmail.com>
2025-03-15 11:06:36 +0000

    Code cleanup

--- src/components/Dashboard/displayContact.jsx
+++ src/components/Dashboard/displayContact.jsx
@@ -161,3 +161,4 @@ export default DisplayContact;// TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
+// TODO: Improve component performance
