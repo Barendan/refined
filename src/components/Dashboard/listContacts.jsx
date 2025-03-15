63be5deef607c753ecbdd5e2f8e8720c66f5ecf2
barendan <barendan@gmail.com>
2025-03-15 14:25:51 +0000

    Improving performance

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -172,3 +172,4 @@ export default ContactList// TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
+// TODO: Improve component performance
