62ac4981127386b9073aaac76214ec1a139cbee3
barendan <barendan@gmail.com>
2025-03-15 14:31:10 +0000

    Code cleanup

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -173,3 +173,4 @@ export default ContactList// TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
+// TODO: Improve component performance
