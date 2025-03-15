c1c848c76050392162cd876c35ecb28e4a0230b8
barendan <barendan@gmail.com>
2025-03-15 14:17:06 +0000

    Refactoring logic

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -171,3 +171,4 @@ export default ContactList// TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
+// TODO: Improve component performance
