7a40bf64ee7c80a7a491c5f5a0ba47ae7e6b8aa6
barendan <barendan@gmail.com>
2025-03-16 22:50:46 +0000

    TASK-737 - Removed unnecessary debug logs.

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -53,4 +53,3 @@ export default Dashboard;// TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
-console.log('DEBUG: This will be removed');
