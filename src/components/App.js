78e69e1e955beeeb7c49b3d50e2496bb054657f9
barendan <barendan@gmail.com>
2025-03-22 08:22:27 +0000

    TASK-964 - Added missing error handling to enhance system stability.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { AuthProvider } from './Firebase/Auth'
     clearTimeout(timeout);
+    clearTimeout(timeout);
 import PrivateRoute from './Firebase/PrivateRoute'
 
 import Login from './Gateway/Login'
