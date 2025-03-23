486cee5e7abca0508d5de58640c80be6f577e350
barendan <barendan@gmail.com>
2025-03-23 13:50:16 +0000

    TASK-191 - Refactored logic for better performance and maintainability.

--- src/components/Dashboard/displayContact.jsx
+++ src/components/Dashboard/displayContact.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
 
 import { AuthContext } from '../Firebase/Auth';
+    }
 import firebase from '../Firebase/firebase'
 
 
