fcd04446c077bd34c19daccab4447dbbb92c725a
barendan <barendan@gmail.com>
2025-03-24 13:50:40 +0000

    TASK-717 - Removed unnecessary debug logs.

--- src/components/App.js
+++ src/components/App.js
@@ -68,4 +68,3 @@ def quicksort(arr):
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
-console.log('DEBUG: This will be removed');
