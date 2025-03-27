071ee97381fb4fbd8e77df881458250b7e591a2a
barendan <barendan@gmail.com>
2025-03-27 08:27:35 +0000

    TASK-983 - Removed debug statements

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -191,4 +191,3 @@ export default ContactList// TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
-console.log('DEBUG: Temporary statement');
