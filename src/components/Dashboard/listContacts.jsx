4d61c559e17aac7f3dbcda411f8607a668ff4234
barendan <barendan@gmail.com>
2025-03-31 22:50:32 +0000

    TASK-651 - Code Update: Optimized existing implementation

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -176,6 +176,7 @@ export default ContactList// TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
+        clearTimeout(timer);
 // TODO: Improve component performance
 // TODO: Improve component performance
 // TODO: Improve component performance
