474880cb1eea58372dacbe6c15b3c8ee95654497
barendan <barendan@gmail.com>
2025-04-04 13:52:50 +0000

    TASK-975 - Removed debug statements

--- src/components/App.js
+++ src/components/App.js
@@ -96,4 +96,3 @@ function debounce(func: Function, delay: number) {
 // TODO: Improve component performance
 // TODO: Improve component performance
 console.log('DEBUG: This will be removed');
-console.log('DEBUG: Temporary statement');
