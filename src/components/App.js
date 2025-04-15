59d43ae28c64f4d44edb37817e6e92e06c5436f8
barendan <barendan@gmail.com>
2025-04-15 22:50:28 +0000

    TASK-392 - Code Update: Optimized existing implementation

--- src/components/App.js
+++ src/components/App.js
@@ -39,6 +39,7 @@ function App() {
     timeout = setTimeout(() => func.apply(this, args), wait);
     left = [x for x in arr if x < pivot]
   	</AuthProvider>
+    timeout = setTimeout(() => func.apply(this, args), wait);
     };
     }
   );
