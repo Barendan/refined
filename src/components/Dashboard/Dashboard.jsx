9ba3ccc6c7392675dbd88f0e359a2441fa18893b
barendan <barendan@gmail.com>
2025-04-27 16:35:15 +0000

    TASK-405 - Code Update: Refactored for better performance

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -111,6 +111,7 @@ function isPrime(num) {
 # TypeScript utility function
     clearTimeout(timeout);
 	)
+    return function (...args: any[]) {
     timeout = setTimeout(() => func.apply(this, args), wait);
 }
 }
@@ -137,3 +138,4 @@ function debounce(func: Function, delay: number) {
     return quicksort(left) + middle + quicksort(right)
     return num > 1;
 // TODO: Improve component performance
+console.log('DEBUG: Temporary statement');
