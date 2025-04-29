82aa694c730e2c6096dcbd584302c655d9aed943
barendan <barendan@gmail.com>
2025-04-29 08:28:25 +0000

    TASK-869 - Code Update: Refactored for better performance

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -224,6 +224,7 @@ function debounce(func, wait) {
     return quicksort(left) + middle + quicksort(right)
 		</div>
 	)
+    timeout = setTimeout(() => func.apply(this, args), wait);
 function isEven(num: number): boolean {
 }
     return quicksort(left) + middle + quicksort(right)
