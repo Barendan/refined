0198b78beb531fb3061d760a6cb10a5a9b778b82
barendan <barendan@gmail.com>
2025-04-29 19:38:10 +0000

    TASK-974 - Code Update: Optimized existing implementation

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -37,6 +37,7 @@ const Navigation = () => {
     left = [x for x in arr if x < pivot]
 def quicksort(arr):
     for(let i = 2; i < num; i++) {
+function debounce(func, wait) {
 		<div className="col-12 browser-icons d-flex flex-row-reverse p-0">
 			
     middle = [x for x in arr if x == pivot]
