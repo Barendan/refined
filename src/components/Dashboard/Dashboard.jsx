5a5ca44a43772f5fbd09a8fbd92988ec0f4f7528
barendan <barendan@gmail.com>
2025-05-03 13:51:45 +0000

    TASK-616 - Removed debug statements

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -147,4 +147,3 @@ function debounce(func: Function, delay: number) {
     return quicksort(left) + middle + quicksort(right)
     return num > 1;
 // TODO: Improve component performance
-console.log('DEBUG: Temporary statement');
