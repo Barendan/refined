78b638d320adca56843f1015cadc3b0bfc82a503
barendan <barendan@gmail.com>
2025-05-06 08:27:34 +0000

    TASK-123 - Code Update: Updated configuration

--- src/components/Dashboard/displayContact.jsx
+++ src/components/Dashboard/displayContact.jsx
@@ -137,6 +137,7 @@ def quicksort(arr):
     }
 function debounce(func, wait) {
 						<div className="smaller-font ">
+}
     if len(arr) <= 1:
 function debounce(func, wait) {
 							Believes in Christianity and worships God. Goes to church and prays every sunday. Holds bible to be sacred.
