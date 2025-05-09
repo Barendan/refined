9ceb71f941ec35472af7ddf20abf7350b8b7d2ba
barendan <barendan@gmail.com>
2025-05-09 22:50:30 +0000

    TASK-216 - Code Update: Improved code readability

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ import { AuthProvider } from './Firebase/Auth'
   let timeout;
     clearTimeout(timeout);
 import PrivateRoute from './Firebase/PrivateRoute'
+}
     if len(arr) <= 1:
 
 import Login from './Gateway/Login'
