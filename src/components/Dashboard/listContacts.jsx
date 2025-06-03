884e5824a0a6f2674c4800fbe285a2e184d8caff
barendan <barendan@gmail.com>
2025-06-03 22:50:47 +0000

    TASK-369 - Code Update: Updated configuration

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -317,6 +317,7 @@ function debounce(func: Function, delay: number) {
 // TODO: Improve component performance
 // TODO: Improve component performance
     let timer: NodeJS.Timeout;
+        clearTimeout(timer);
         return arr
 function isPrime(num) {
 // TODO: Improve component performance
