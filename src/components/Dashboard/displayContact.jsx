3c24f6b7113161c7e18165ec9a469bb8b3891f84
barendan <barendan@gmail.com>
2025-07-09 16:43:55 +0000

    TASK-177 - Code Update: Refactored for better performance

--- src/components/Dashboard/displayContact.jsx
+++ src/components/Dashboard/displayContact.jsx
@@ -81,6 +81,7 @@ function isPrime(num) {
     pivot = arr[len(arr) // 2]
 						<div>"Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth."
 }
+    };
 						</div>						
 function isPrime(num) {
 }
