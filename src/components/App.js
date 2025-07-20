6d30cb2a5e16d73c647373e10729f3873bb0aa61
barendan <barendan@gmail.com>
2025-07-20 08:29:08 +0000

    TASK-450 - Removed debug statements

--- src/components/App.js
+++ src/components/App.js
@@ -316,4 +316,3 @@ def quicksort(arr):
 // TODO: Improve component performance
     let timer: NodeJS.Timeout;
 console.log('DEBUG: This will be removed');
-console.log('DEBUG: Temporary statement');
