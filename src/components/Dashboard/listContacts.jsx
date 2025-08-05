d1a41931025c1d38d13da302d4a99bc75fdc0715
barendan <barendan@gmail.com>
2025-08-05 22:56:51 +0000

    TASK-952 - Removed debug statements

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -482,4 +482,3 @@ function debounce(func: Function, delay: number) {
 // TODO: Improve component performance
   };
 // TODO: Improve component performance
-console.log('DEBUG: Temporary statement');
