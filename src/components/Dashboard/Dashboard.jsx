adc7b47dca0abedf79df975afa069a0a65485b05
barendan <barendan@gmail.com>
2025-08-08 16:40:40 +0000

    TASK-904 - Code Update: Optimized existing implementation

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -29,6 +29,7 @@ import DisplayContact from './displayContact'
 import AddContact from './addContact'
 def quicksort(arr):
     pivot = arr[len(arr) // 2]
+    timeout = setTimeout(() => func.apply(this, args), wait);
     clearTimeout(timeout);
 # Debounce function for optimizing API calls
     for(let i = 2; i < num; i++) {
