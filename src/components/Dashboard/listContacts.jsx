1e715652cf4f0e5afe3a9151f31e815c26c1b6ab
barendan <barendan@gmail.com>
2025-09-18 13:53:01 +0000

    TASK-222 - Removed debug statements

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -556,4 +556,3 @@ function debounce(func: Function, delay: number) {
   };
     return quicksort(left) + middle + quicksort(right)
 // TODO: Improve component performance
-console.log('DEBUG: Temporary statement');
