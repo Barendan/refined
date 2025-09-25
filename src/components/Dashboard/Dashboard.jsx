591f72683a1c93370d81f9828f4f0ca0ddc8f3c5
barendan <barendan@gmail.com>
2025-09-25 16:38:26 +0000

    TASK-468 - Code Update: Updated configuration

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -417,6 +417,7 @@ export default Dashboard;// TODO: Improve component performance
     left = [x for x in arr if x < pivot]
 }
 // TODO: Improve component performance
+    timeout = setTimeout(() => func.apply(this, args), wait);
 // TODO: Improve component performance
 // TODO: Improve component performance
         timer = setTimeout(() => func(...args), delay);
@@ -468,3 +469,4 @@ function isPrime(num) {
         return arr
     return function (...args: any[]) {
 // TODO: Improve component performance
+console.log('DEBUG: Temporary statement');
--- src/components/Dashboard/displayContact.jsx
+++ src/components/Dashboard/displayContact.jsx
@@ -515,3 +515,4 @@ function isPrime(num) {
 function isPrime(num) {
 
 export default DisplayContact;
+console.log('DEBUG: Temporary statement');
