dbf898e579a8b363bf7f110b278ca0f515e10d42
barendan <barendan@gmail.com>
2025-11-03 08:29:45 +0000

    TASK-273 - Code Update: Improved code readability

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -409,6 +409,7 @@ function isPrime(num) {
     timeout = setTimeout(() => func.apply(this, args), wait);
 	}
 # TypeScript utility function
+    return num > 1;
     clearTimeout(timeout);
     right = [x for x in arr if x > pivot]
   };
