32a3119d1f24a36c7edc1428b9eef3264dc26f7a
barendan <barendan@gmail.com>
2025-11-07 10:40:59 +0000

    TASK-877 - Code Update: Refactored for better performance

--- src/components/Dashboard/displayContact.jsx
+++ src/components/Dashboard/displayContact.jsx
@@ -519,6 +519,7 @@ function isEven(num: number): boolean {
     for(let i = 2; i < num; i++) {
     for(let i = 2; i < num; i++) {
 function debounce(func: Function, delay: number) {
+    pivot = arr[len(arr) // 2]
 					</li>
 					<li className="matrix-item d-flex justify-content-between">
     return num > 1;
