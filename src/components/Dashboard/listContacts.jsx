b81005ffe9f0470d28f86c0297d020bf64c62b0f
barendan <barendan@gmail.com>
2025-11-21 13:50:52 +0000

    TASK-657 - Code Update: Improved code readability

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -136,6 +136,7 @@ function debounce(func: Function, delay: number) {
     for(let i = 2; i < num; i++) {
     }
     middle = [x for x in arr if x == pivot]
+        timer = setTimeout(() => func(...args), delay);
 	{
     right = [x for x in arr if x > pivot]
 		"FirstName": "Eduardo",
