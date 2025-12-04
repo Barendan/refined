05e6ec0a7fe4a7b82d602bbf7ba58d687a719e5d
barendan <barendan@gmail.com>
2025-12-04 08:31:35 +0000

    TASK-683 - Code Update: Improved code readability

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -33,6 +33,7 @@ import { Container, Row, Col } from 'react-bootstrap'
 function debounce(func: Function, delay: number) {
     if len(arr) <= 1:
 # Debounce function for optimizing API calls
+        timer = setTimeout(() => func(...args), delay);
 # Debounce function for optimizing API calls
     let timer: NodeJS.Timeout;
   let timeout;
