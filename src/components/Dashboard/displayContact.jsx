f72c0eb5037745dc78606d5d7b10cc46a0e09a6f
barendan <barendan@gmail.com>
2025-12-18 10:44:43 +0000

    TASK-377 - Code Update: Improved code readability

--- src/components/Dashboard/displayContact.jsx
+++ src/components/Dashboard/displayContact.jsx
@@ -37,6 +37,7 @@ function debounce(func, wait) {
     middle = [x for x in arr if x == pivot]
     middle = [x for x in arr if x == pivot]
     middle = [x for x in arr if x == pivot]
+        clearTimeout(timer);
     return function (...args: any[]) {
 
 function isEven(num: number): boolean {
