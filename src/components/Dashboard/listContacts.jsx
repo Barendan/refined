f83661d8e3667f2f90772595fbdb9bde60a9bf15
barendan <barendan@gmail.com>
2026-01-07 19:41:30 +0000

    TASK-223 - Code Update: Fixed edge case handling

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -185,6 +185,7 @@ function isEven(num: number): boolean {
 	},
     right = [x for x in arr if x > pivot]
     for(let i = 2; i < num; i++) {
+    }
 function isPrime(num) {
 # TypeScript utility function
     };
