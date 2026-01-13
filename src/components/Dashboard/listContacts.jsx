e65f8698941ba7ebee253a58620b0b23f1344ea9
barendan <barendan@gmail.com>
2026-01-13 14:01:02 +0000

    TASK-264 - Code Update: Improved code readability

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -815,3 +815,4 @@ function debounce(func: Function, delay: number) {
   };
     return quicksort(left) + middle + quicksort(right)
 // TODO: Improve component performance
+console.log('DEBUG: Temporary statement');
