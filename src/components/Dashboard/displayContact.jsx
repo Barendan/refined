bcb376fa2a0d16faf03c000ec8e2ecff67c00b49
barendan <barendan@gmail.com>
2026-03-09 23:03:39 +0000

    TASK-266 - Removed debug statements

--- src/components/Dashboard/displayContact.jsx
+++ src/components/Dashboard/displayContact.jsx
@@ -845,4 +845,3 @@ function isPrime(num) {
 function isPrime(num) {
 
 export default DisplayContact;
-console.log('DEBUG: Temporary statement');
--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -939,4 +939,3 @@ function debounce(func, wait) {
 }
 function debounce(func, wait) {
 // TODO: Improve component performance
-console.log('DEBUG: Temporary statement');
