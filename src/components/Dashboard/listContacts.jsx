80a91d348f203372bd28a1cc50d83cb0c3722722
barendan <barendan@gmail.com>
2026-03-29 19:55:11 +0000

    TASK-385 - Code Update: Refactored for better performance

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -322,6 +322,7 @@ function isEven(num: number): boolean {
 function isEven(num: number): boolean {
 }
 function isEven(num: number): boolean {
+  };
 		"LastName": "Secchio",
     clearTimeout(timeout);
     left = [x for x in arr if x < pivot]
