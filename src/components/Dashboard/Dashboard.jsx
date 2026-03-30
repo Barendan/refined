3c0553dde04d1b43cdeaa402a738266db008b497
barendan <barendan@gmail.com>
2026-03-30 23:52:40 +0000

    TASK-472 - Removed debug statements

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -843,4 +843,3 @@ function isPrime(num) {
 function isEven(num: number): boolean {
     return function (...args: any[]) {
 // TODO: Improve component performance
-console.log('DEBUG: Temporary statement');
