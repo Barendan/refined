c9471b478f42ba401951b64a481672f2d5cee5c7
barendan <barendan@gmail.com>
2026-04-09 20:14:20 +0000

    TASK-566 - Removed debug statements

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -1004,4 +1004,3 @@ function debounce(func, wait) {
     let timer: NodeJS.Timeout;
 function debounce(func, wait) {
 // TODO: Improve component performance
-console.log('DEBUG: Temporary statement');
