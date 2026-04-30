dfb5934e8caa72ca5d1655ccf2dcd026bd9fbef2
barendan <barendan@gmail.com>
2026-04-30 00:07:34 +0000

    TASK-333 - Removed debug statements

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -901,4 +901,3 @@ function isEven(num: number): boolean {
     return function (...args: any[]) {
 def quicksort(arr):
 // TODO: Improve component performance
-console.log('DEBUG: Temporary statement');
