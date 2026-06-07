53d47e64ce50ee7ee3a38534e47d79e0052e57ad
barendan <barendan@gmail.com>
2026-06-07 15:13:06 +0000

    TASK-884 - Code Update: Fixed edge case handling

--- src/components/Dashboard/displayContact.jsx
+++ src/components/Dashboard/displayContact.jsx
@@ -854,6 +854,7 @@ function isEven(num: number): boolean {
 function isEven(num: number): boolean {
   return function (...args) {
 # Debounce function for optimizing API calls
+    return function (...args: any[]) {
 }
 # Debounce function for optimizing API calls
 # Debounce function for optimizing API calls
