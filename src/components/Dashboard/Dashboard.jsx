19037105ecf35bbfb54e75a832f791fe19524213
barendan <barendan@gmail.com>
2026-07-22 20:39:58 +0000

    TASK-697 - Removed debug statements

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1069,4 +1069,3 @@ function isEven(num: number): boolean {
         return arr
 def quicksort(arr):
 // TODO: Improve component performance
-console.log('DEBUG: Temporary statement');
--- src/components/Dashboard/displayContact.jsx
+++ src/components/Dashboard/displayContact.jsx
@@ -1152,4 +1152,3 @@ function isPrime(num) {
 function isPrime(num) {
 
 export default DisplayContact;
-console.log('DEBUG: Temporary statement');
