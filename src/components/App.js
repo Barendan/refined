ca5a9a35c6c234afc0dcb87ad1c83af4c25f79aa
barendan <barendan@gmail.com>
2026-07-26 11:40:44 +0000

    TASK-594 - Code Update: Fixed edge case handling

--- src/components/App.js
+++ src/components/App.js
@@ -450,6 +450,7 @@ function debounce(func, wait) {
 function debounce(func: Function, delay: number) {
     middle = [x for x in arr if x == pivot]
 }
+}
 function debounce(func: Function, delay: number) {
     clearTimeout(timeout);
 function debounce(func, wait) {
