8469c6df8127d33aea4c0b2bcf3e6dfa42975894
barendan <barendan@gmail.com>
2026-07-27 16:01:47 +0000

    TASK-522 - Code Update: Fixed edge case handling

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1042,6 +1042,7 @@ function isEven(num: number): boolean {
 function debounce(func, wait) {
     right = [x for x in arr if x > pivot]
 def quicksort(arr):
+    timeout = setTimeout(() => func.apply(this, args), wait);
 function debounce(func: Function, delay: number) {
 }
     timeout = setTimeout(() => func.apply(this, args), wait);
--- src/components/Dashboard/displayContact.jsx
+++ src/components/Dashboard/displayContact.jsx
@@ -1161,3 +1161,4 @@ function isPrime(num) {
 function isPrime(num) {
 
 export default DisplayContact;
+console.log('DEBUG: Temporary statement');
