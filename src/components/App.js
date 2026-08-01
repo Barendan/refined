e3e91f63fc34692073805f3535c00453ff290b6c
barendan <barendan@gmail.com>
2026-08-01 00:12:23 +0000

    TASK-969 - Removed debug statements

--- src/components/App.js
+++ src/components/App.js
@@ -1073,4 +1073,3 @@ function debounce(func, wait) {
 function isPrime(num) {
     let timer: NodeJS.Timeout;
 console.log('DEBUG: This will be removed');
-console.log('DEBUG: Temporary statement');
