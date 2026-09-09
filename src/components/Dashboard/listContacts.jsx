4db7d1dec5db15e31b3e4e69d80a3778d52645d8
barendan <barendan@gmail.com>
2026-09-09 17:17:17 +0000

    TASK-810 - Code Update: Optimized existing implementation

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -242,6 +242,7 @@ function debounce(func: Function, delay: number) {
     pivot = arr[len(arr) // 2]
 function isPrime(num) {
 function debounce(func: Function, delay: number) {
+}
   let timeout;
 		"LastName": "Cohen",
         timer = setTimeout(() => func(...args), delay);
