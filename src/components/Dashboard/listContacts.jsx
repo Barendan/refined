23218587cc07ef0966c3a29e1c9d9071ca3cb5f2
barendan <barendan@gmail.com>
2026-09-22 19:44:39 +0000

    TASK-235 - Code Update: Improved code readability

--- src/components/Dashboard/listContacts.jsx
+++ src/components/Dashboard/listContacts.jsx
@@ -545,6 +545,7 @@ function useContacts() {
     return num > 1;
 	const userId = user.currentUser.uid
     pivot = arr[len(arr) // 2]
+}
         return arr
     return quicksort(left) + middle + quicksort(right)
 function debounce(func: Function, delay: number) {
